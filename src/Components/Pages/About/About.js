b0e193701bacdb91301ced661fae9deb6a4d2534
aliarman78 <aliarman787878@gmail.com>
2022-04-18 22:50:26 +0600

    About page text

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (< div >
         <h1 className='text-center'> About Me </h1>
-        
+        <h4 className='container mb-5'>Hi, I am Ali Arman from Chittagong, Bangladesh. I study Computer Science Technology at (Weifang University of Science and Technology) in China. I am a Professorial WordPress Expert as well as Shopify Expert. I have been working in Graphic Design and Digital Marketing and also I know Microsoft Word, Microsoft Excel and Microsoft PowerPoint Slide Presentation. I have done a lot of work locally and internationally. I love work properly as It's my passion. Now I'm learning Fontend Development at Programming Hero. Learning React-JS, Firebase, JavaScript, HTML, CSS etc.</h4>
     </div>
 
     );
