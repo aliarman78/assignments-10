df85184e89ad800abfc662aabc8f11e51a90836f
aliarman78 <aliarman787878@gmail.com>
2022-04-18 22:45:53 +0600

    2nd ans done

--- src/Components/Pages/Blog/BLog.js
+++ src/Components/Pages/Blog/BLog.js
@@ -17,7 +17,19 @@ const BLog = () => {
                 <h5> 1. Why are you using firebase ? What other options do you have to implement authentication ?</h5>
                 <p> Answer : As a front-end developer, learning back-end technologies is probably not your original or main goal, and it can be time consuming. However, there is something that can help called Firebase. Firebase is a software development platform that started as a realtime database. To-date it has 20 services, such as Authentication, Cloud Storage, Hosting, Cloud Functions, etc.<br />
                 </p> </Col>
-           
+            < Col >
+                <h5> 2. What other services does firebase provide other than authentication ? </h5> <p> Answer: </p>
+                <ul>
+                    <li> Cloud Firestore, </li>
+                    <li> Cloud Functions, </li>
+                    <li> Cloud Storage, </li>
+                    <li> Google Analytics, </li>
+                    <li> Cloud Messaging, </li>
+                    <li> Authentication, </li>
+                    <li> Hosting, </li>
+                    <li> Couchbase etc. </li>
+                </ul>
+            </Col>
             
         </Row> </div>
     );
